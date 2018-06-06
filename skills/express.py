885db7ed1c31ff8d0d2d6a82cf049a9fa6e1318c
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-06 13:01:53 +0800

    Create express.py

---
+++ skills/express.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+'''快递查询技能'''
+
+import sys
+import os
+import requests
+import json
+import demjson
+
+import speaker
+from tts import baidu_tts
+from tts import youdao_tts
+from stt import baidu_stt
+from recorder import recorder
+
+def start(tok):
+
+    main(tok)
+    
+def number_choose(text, tok):
+
+
+
+def main(tok):
