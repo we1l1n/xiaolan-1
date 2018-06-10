117f3d555c146e88c8a6597a142180f4839bf791
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-10 15:34:17 +0800

    Update snowboy.py

--- snowboy.py
+++ snowboy.py
@@ -5,10 +5,13 @@ import pyaudio
 sys.path.append('/home/pi/xiaolan/snowboy')
 import snowboydecoder
 
-def awaken():
+class sb(object):
+    def __init__(self):
+        pass
+    def start(self):
 
-    detector = snowboydecoder.HotwordDetector("/home/pi/xiaolan/snowboy/Alexa.pmdl", sensitivity=0.5, audio_gain=1)
-    detector.start(detected_callback)
+        detector = snowboydecoder.HotwordDetector("/home/pi/xiaolan/snowboy/Alexa.pmdl", sensitivity=0.5, audio_gain=1)
+        detector.start(detected_callback)
 
 def detected_callback():
     print "检测到唤醒，转交指令给xldo"
@@ -16,7 +19,5 @@ def detected_callback():
         sys.exit(-1)
     except SystemExit:
         os.system('python /home/pi/xiaolan/xldo.py convenstation')
-
-awaken()
 
     
