551cdaf2f404e2c5e8bf2283f8405004329fc222
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-10 22:15:02 +0800

    Update maps.py

--- skills/maps.py
+++ skills/maps.py
@@ -26,14 +26,21 @@ class maps(object):
     
     def __init__(self):
       
-        pass
+        self.set = setting.setting()
+    
+    def ip_loc(self, tok):
+        
+        r = requests.ger('http://restapi.amap.com/v3/ip?' + 'output=json&key=' + self.set['maps']['key'])
+        try:
+            bt.tts('你在' + r.json()['province'] + r.json()['city'], tok)
+            speaker.speak()
+        except KeyError:
+            bt.tts('位置获取失败', tok)
+            speaker.speak()
     
     def near(self, place, tok):
         
-        selfset = setting.setting()
-        citys = requests.get('http://apis.map.qq.com/ws/location/v1/ip')
-        nears = requests.get('http://apis.map.qq.com/ws/place/v1/search?' + 'boundary=region(' + city + ',0)&keyword=' + place + '&page_size=5&page_index=1&orderby=_distance&key=' + setting['map']['key'])
-        
+        pass
         
     def go_way(self, place_f, place_s, tok):
         
@@ -44,9 +51,9 @@ class maps(object):
         bt = baidu_tts()
         bs = baidu_stt(1, 2, 3, 4)
         r = recorder()
-        m = map()
+        m = maps()
         
-        bt.tts('欢迎使用小蓝地图技能，API服务由腾讯地图提供', tok)
+        bt.tts('欢迎使用小蓝地图技能', tok)
         speaker.speak()
         bt.tts('支持查找路线，附近有什么服务等，如：从这里到美丽公园怎么走？，又比如：美丽公园附近有什么吃的？', tok)
         speaker.speak()
@@ -60,14 +67,16 @@ class maps(object):
         
     def choose_command(self, text, tok):
         
-        m = map()
+        m = maps()
         bt = baidu_tts()
         bs = baidu_stt(1, 2, 3, 4)
         r = recorder()
         if '从' in text and '怎么去' in text:
-            m.go_way(text[0:-7], text[-8:-3], tok)
+            m.go_way(text[0:-7], text[-8:-4], tok)
+        elif '我在哪' in text or '我的位置,' == text:
+            m.ip_loc(tok)
         elif '从' in text and '怎么走' in text:
-            m.go_way(text[0:-7], text[-8:-3], tok)
+            m.go_way(text[0:-7], text[-8:-4], tok)
         elif '附近' in text:
             m.near(text[0:5], tok)
         else:
