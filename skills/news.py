ec66c61419ac4351b4b2fe68c4262e5cd3ed22ac
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-10 10:56:35 +0800

    Update news.py

--- skills/news.py
+++ skills/news.py
@@ -4,7 +4,6 @@ import json
 import sys
 import os
 import requests
-import pygame
 import time
 import random
 sys.path.append('/home/pi/xiaolan/')
@@ -32,155 +31,31 @@ def main(tok):
     r.record()
     speaker.dong()
     text = bs.stt('./voice.wav', tok)
-    chose = random.randint(0,9)
     
     if '国内新闻' in text:
-        newsservice = 'chinanews'
+        r = requests.post(url + 'guonei' + '&key=' + APPKEY)
+        json = r.json()
+        bt.tts(json['result']['data'][random.randint(0,9)]['title'], tok)
+        speaker.speak()
     elif '国际新闻' in text:
-        newsservice = 'nationnews'
+        r = requests.post(url + 'guoji' + '&key=' + APPKEY)
+        json = r.json()
+        saytext = json['result']['data'][random.randint(0,9)]['title']
+        bt.tts(saytext, tok)
+        speaker.speak()
     elif '科技新闻' in text:
-        newsservice = 'kejinews'
+        r = requests.post(url + 'keji' + '&key=' + APPKEY)
+        json = r.json()
+        bt.tts(json['result'][random.randint(0,9)]['title'], tok)
+        speaker.speak()
     elif '体育新闻' in text:
-        newsservice = 'tiyunews'
-    else:
-        newsservice = 't'
-    
-    if newsservice == 'chinanews':
-        newstype = 'guonei'
-        r = requests.post(url + newstype + '&key=' + APPKEY)
+        r = requests.post(url + 'tiyu' + '&key=' + APPKEY)
         json = r.json()
-        if chose == 0:
-            saytext = json['result']['data'][0]['title']
-        elif chose == 1:
-            saytext = json['result']['data'][1]['title']
-        elif chose == 2:
-            saytext = json['result']['data'][2]['title']
-        elif chose == 3:
-            saytext = json['result']['data'][3]['title']
-        elif chose == 4:
-            saytext = json['result']['data'][4]['title']
-        elif chose == 5:
-            saytext = json['result']['data'][5]['title']
-        elif chose == 6:
-            saytext = json['result']['data'][6]['title']
-        elif chose == 7:
-            saytext = json['result']['data'][7]['title']
-        elif chose == 8:
-            saytext = json['result']['data'][8]['title']
-        elif chose == 9:
-            saytext = json['result']['data'][9]['title']
-        else:
-            saytext = json['result']['data'][0]['title']
-        bt.tts(saytext, tok)
+        bt.tts(json['result'][random.randint(0,9)]['title'], tok)
         speaker.speak()
-        
-    elif newsservice == 'nationnews':
-        newstype = 'guoji'
-        r = requests.post(url + newstype + '&key=' + APPKEY)
-        json = r.json()
-        if chose == 0:
-            saytext = json['result']['data'][0]['title']
-        elif chose == 1:
-            saytext = json['result']['data'][1]['title']
-        elif chose == 2:
-            saytext = json['result']['data'][2]['title']
-        elif chose == 3:
-            saytext = json['result']['data'][3]['title']
-        elif chose == 4:
-            saytext = json['result']['data'][4]['title']
-        elif chose == 5:
-            saytext = json['result']['data'][5]['title']
-        elif chose == 6:
-            saytext = json['result']['data'][6]['title']
-        elif chose == 7:
-            saytext = json['result']['data'][7]['title']
-        elif chose == 8:
-            saytext = json['result']['data'][8]['title']
-        elif chose == 9:
-            saytext = json['result']['data'][9]['title']
-        else:
-            saytext = json['result']['data'][0]['title']
-        bt.tts(saytext, tok)
-        speaker.speak()
-    
-    elif newsservice == 'kejinews':
-        newstype = 'keji'
-        r = requests.post(url + newstype + '&key=' + APPKEY)
-        json = r.json()
-        if chose == 0:
-            saytext = json['result']['data'][0]['title']
-        elif chose == 1:
-            saytext = json['result']['data'][1]['title']
-        elif chose == 2:
-            saytext = json['result']['data'][2]['title']
-        elif chose == 3:
-            saytext = json['result']['data'][3]['title']
-        elif chose == 4:
-            saytext = json['result']['data'][4]['title']
-        elif chose == 5:
-            saytext = json['result']['data'][5]['title']
-        elif chose == 6:
-            saytext = json['result']['data'][6]['title']
-        elif chose == 7:
-            saytext = json['result']['data'][7]['title']
-        elif chose == 8:
-            saytext = json['result']['data'][8]['title']
-        elif chose == 9:
-            saytext = json['result']['data'][9]['title']
-        else:
-            saytext = json['result']['data'][0]['title']
-    elif newsservice == 'tiyunews':
-        newstype = 'tiyu'
-        r = requests.post(url + newstype + '&key=' + APPKEY)
-        json = r.json()
-        if chose == 0:
-            saytext = json['result']['data'][0]['title']
-        elif chose == 1:
-            saytext = json['result']['data'][1]['title']
-        elif chose == 2:
-            saytext = json['result']['data'][2]['title']
-        elif chose == 3:
-            saytext = json['result']['data'][3]['title']
-        elif chose == 4:
-            saytext = json['result']['data'][4]['title']
-        elif chose == 5:
-            saytext = json['result']['data'][5]['title']
-        elif chose == 6:
-            saytext = json['result']['data'][6]['title']
-        elif chose == 7:
-            saytext = json['result']['data'][7]['title']
-        elif chose == 8:
-            saytext = json['result']['data'][8]['title']
-        elif chose == 9:
-            saytext = json['result']['data'][9]['title']
-        else:
-            saytext = json['result']['data'][0]['title']
     else:
-        newstype = 'top'
-        r = requests.post(url + newstype + '&key=' + APPKEY)
+        r = requests.post(url + 'top' + '&key=' + APPKEY)
         json = r.json()
-        if chose == 0:
-            saytext = json['result'][0]['title']
-        elif chose == 1:
-            saytext = json['result'][1]['title']
-        elif chose == 2:
-            saytext = json['result'][2]['title']
-        elif chose == 3:
-            saytext = json['result'][3]['title']
-        elif chose == 4:
-            saytext = json['result'][4]['title']
-        elif chose == 5:
-            saytext = json['result'][5]['title']
-        elif chose == 6:
-            saytext = json['result'][6]['title']
-        elif chose == 7:
-            saytext = json['result'][7]['title']
-        elif chose == 8:
-            saytext = json['result'][8]['title']
-        elif chose == 9:
-            saytext = json['result'][9]['title']
-        else:
-            saytext = json['result'][0]['title']
-        bt.tts(saytext, tok)
+        bt.tts(json['result'][random.randint(0,9)]['title'], tok)
         speaker.speak()
     
