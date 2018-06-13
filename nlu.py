218b936b643c9a47b865e67c148d437e47683571
Lanzhijiang <37663413+xiaoland@users.noreply.github.com>
2018-06-13 22:18:13 +0800

    Update nlu.py

--- nlu.py
+++ nlu.py
@@ -47,8 +47,8 @@ class Nlu(Xiaolan):
                         ['weather', ['天气', '天气怎么样', '查询天气', '今天天气'], ['city', self.dictcity], 'weather'],
                         ['talk', ['我想跟你聊一聊', '我想聊你'], [], 'tuling'],
                         ['joke', ['我想听笑话', '笑话', '冷笑话', '给我讲一个笑话'], [], 'joke'],
-                        ['news', ['我想听新闻', '今天的新闻', '新闻', '今天有什么新闻'], 'news'],
-                        ['smarthome', ['打开', '关闭', '开启', '获取', '传感器', '智能家居'], 'hass'],
+                        ['news', ['我想听新闻', '今天的新闻', '新闻', '今天有什么新闻'], ['newstype', self.dictnewstype] 'news'],
+                        ['smarthome', ['打开', '关闭', '开启', '获取', '传感器', '智能家居'], ['mode', self.dicthassmode, 'cortolmode', self.dicthasscortolmode, 'device': self.dictdevice], 'hass'],
                         ['clock', ['设定一个闹钟', '闹钟', '设置新闹钟', '新建闹钟'], ['day', self.dictday, 'weekday', self.dictweekday, 'hour', self.dicthour, 'minute', self.dictminute], 'clock']
                 ]
                 self.music_service = {'musicurl_get': 'method=baidu.ting.song.play&songid=', 'search': 'method=baidu.ting.search.catalogSug&query=', 'hot': 'method=baidu.ting.song.getRecommandSongList&song_id=877578&num=12'}
